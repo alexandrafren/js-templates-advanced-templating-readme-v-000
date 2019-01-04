ca5fe79a3057ba215e6bd3b2dcc0d0c4ac09ee4a
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-04 20:06:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function loadIssue() {
   var template =
   Handlebars.compile(document.getElementById("issue-template").innerHTML);
 
-  var result = template(issue);
+  var result = template(issues);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
